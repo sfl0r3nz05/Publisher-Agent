b0f50ab78c73160f4f61f98120bae76f2d83f8c4
sfl0r3nz05 <santiagofigueroalorenzo@gmail.com>
2023-04-29 21:12:06 +0000

    git submodule updated

--- src/models/DataGui.js
+++ src/models/DataGui.js
@@ -1,14 +1,14 @@
 class DataGui {
-    constructor(ID_mensaje, timestamp, tagID, x, y, z, hpl_1, vpl,SingularMatrix) {
-        this.ID_mensaje = ID_mensaje
+    constructor(ID_mensaje, timestamp, tagID, x, y, z, hpl_1, vpl, SingularMatrix) {
+        this.ID_mensaje = Number(ID_mensaje)
         this.timestamp = timestamp
         this.tagID = tagID
-        this.x = x
-        this.y = y
-        this.z = z
-        this.hpl_1 = hpl_1
-        this.vpl = vpl
-        this.SingularMatrix = SingularMatrix
+        this.x = parseFloat(x)
+        this.y = parseFloat(y)
+        this.z = parseFloat(z)
+        this.hpl_1 = parseFloat(hpl_1)
+        this.vpl = parseFloat(vpl)
+        this.SingularMatrix = Number(SingularMatrix)
     }
     
 }
